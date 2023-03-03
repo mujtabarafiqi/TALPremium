1a411f468b46b07b4fca18deade65ac6f1608926
Mujtaba Hyder Rafiqi <mujtaba.rafiqi@lelafe.com>
2023-03-04 00:48:25 +0530

    Entity/Models added

---
+++ Models/Rating.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TALPremium.Models
+{
+    public class Rating : BaseEntity
+    {
+        public decimal Factor { get; set; }
+
+        [InverseProperty(nameof(Occupation.Rating))]
+        public ICollection<Occupation> Occupations { get; set; }
+    }
+}
