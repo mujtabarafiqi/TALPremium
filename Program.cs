6f661fe7df38eb6a8e4789911140ef2201b8966f
Mujtaba Hyder Rafiqi <mujtaba.rafiqi@lelafe.com>
2023-03-04 13:43:23 +0530

    Database seed/initialization of data to default values

--- Program.cs
+++ Program.cs
@@ -40,7 +40,23 @@ namespace TALPremium
                 pattern: "{controller}/{action=Index}/{id?}");
 
             app.MapFallbackToFile("index.html");
+            using (var scope = app.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<TALDbContext>();
 
+                    DbInitializer.Initialize(context);
+
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while seeding the database.");
+                    System.Diagnostics.Trace.WriteLine(ex.StackTrace);
+                }
+            }
             app.Run();
         }
     }
